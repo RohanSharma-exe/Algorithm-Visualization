6a8f746c389929f842a2d77e0514d0c77c7351e1
RohanSharma <68742910+RohanSharma-exe@users.noreply.github.com>
2024-05-25 23:37:30 +0530

    Update dfs.py

--- DFS Algorithm/dfs.py
+++ DFS Algorithm/dfs.py
@@ -6,7 +6,7 @@ pygame.init()
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("BFS Path Finding Algorithm")
+pygame.display.set_caption("DFS Path Finding Algorithm")
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
